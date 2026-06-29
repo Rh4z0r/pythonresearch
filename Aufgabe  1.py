7c943ee2afd6c3d855485e647a5aa782d6efbce8
Rh4z0r <123068227+Rh4z0r@users.noreply.github.com>
2026-06-29 13:39:23 +0000

    added spaces

--- Aufgabe  1.py
+++ Aufgabe  1.py
@@ -4,11 +4,11 @@
 
 # Falls die Eingabe keine Zahl ist, soll die Meldung: "Bitte geben Sie eine Zahl ein" erscheinen und die Eingabe wiederholt werden.
 
-zahl = int(input("bitte gib eine Zahl ein, ich prüfe ob sie gerade oder ungerade ist:"))       # Variable für die erste Zahlenabfrage 
+zahl = int(input("bitte gib eine Zahl ein, ich prüfe ob sie gerade oder ungerade ist: "))       # Variable für die erste Zahlenabfrage 
 print(f"Die Zahl ist {zahl}.")                                                                 # Ausgabe der ersten Zahl 
 
 while zahl % 2 == 0:                                                                           # while Schleife. Condition % 2 wird geprüft
-    zahl = int(input("Die Zahl ist gerade. Bitte eine weitere Zahl eingeben:"))                # Überschreiben der Variablen durch durch einen neuen Input
+    zahl = int(input("Die Zahl ist gerade. Bitte eine weitere Zahl eingeben: "))                # Überschreiben der Variablen durch durch einen neuen Input
     print(f"Die Zahl ist {zahl}.")                                                             # Ausgabe jeder weiteren Eingabe
 
 print("Die eingegebene Zahl ist ungerade. Programm wird verlassen")                            # Ausgabe zum Programmende für ungerade Zahlen
