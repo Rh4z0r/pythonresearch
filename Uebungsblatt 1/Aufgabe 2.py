07e88da5ed00d7aff2402a3179dfcdfb0fe3503d
Rh4z0r <123068227+Rh4z0r@users.noreply.github.com>
2026-06-30 13:19:17 +0000

    Uebungsblatt 1 Aufgabe 2

--- Uebungsblatt 1/Aufgabe 2.py
+++ Uebungsblatt 1/Aufgabe 2.py
@@ -7,3 +7,20 @@
 # Ab dann entspricht ein Hundejahr jeweils 5 Menschenjahren.
 # Schreiben Sie ein Programm, das das Alter eines Hundes erfragt und dann nach obiger Methode
 # berechnet, welchem Alter in Menschenjahren dies entspricht.
+
+try:                                                                                                                                      # Eingabevalidierung
+    human = int(input("Bitte geben sie die gewünschte Anzahl an Menschenjahren ein. Ich werde sie dann in Hundejahre umrechnen: "))       # Abfrage Menschenjahre
+
+    if human < 0:                                                                                                                         # Fehlermeldung bei negativer Zahl
+        print("Bitte geben Sie eine gültige Zahl ein.")                                                                                   # Ausgabe der Fehlermeldung bei negativer Zahl 
+    if human == 0:                                                                                                                        # Bei Eingabe 0 
+        print("0 Menschenjahre entsprechen 0 Hundejahren")                                                                                # Ausgabe bei Zahl 0 
+    elif human == 1:                                                                                                                      # Vergleich für 1 Menschenjahr
+        print("1 Jahr entspricht genau 14 Hundejahren")                                                                                   # Ausgabe für 1 Menschenjahr
+    elif human == 2:                                                                                                                      # Vergleich für 2 Menschenjahre
+        print("2 Jahre entsprechen exakt 22 Hundejahren")                                                                                 # Ausgabe für 2 Menschenjahre
+    else:                                                                                                                                 
+        dogyears = ((human -2) * 5) + 22                                                                                                  # Vergleich für mehr als 2 Menschenjahre
+        print(f"{human} Menschenjahre entsprechen ganz genau {dogyears} Hundejahren.")                                                    # Berechnung der Hundejahre
+except:                                                                                                                                   # Eingabevalidierung
+    print("Das war leider keine gültige Zahl. Bitte versuche es nochmal.")                                                                # Fehlermeldung bei Falscheingaben
